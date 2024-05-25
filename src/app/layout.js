32ca880a210c84ea9f25a9b632099ab90889f5e1
kirankumar6676 <kumarkiran0893@gmail.com>
2024-05-25 22:40:56 +0530

    added name

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Harini's Portfolio",
+  title: "Harinni's Portfolio",
   description: 'This is the portfolio',
 }
 
